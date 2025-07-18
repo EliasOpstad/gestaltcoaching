7265bf9bdf52ae5bd822fe689c33d8e84734886e
Elias Opstad <eops@hotmail.no>
2025-07-18 13:47:39 +0200

    Styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const header = () => {
   return (
-    <div className='flex justify-between items-center p-4 bg-gray-800 text-white fixed top-0 left-0 right-0 z-50 h-[100px]'>
-        {/* Left header "Gestalcoach" */}
-        <h1><a href="/">Gestaltcoach</a></h1>
+    <div className="flex justify-between sticky items-center p-4 bg-gray-800 text-white top-0 left-0 right-0 z-50 h-[100px]">
+      {/* Left header "Gestalcoach" */}
+      <h1>
+        <a href="/">Gestaltcoach</a>
+      </h1>
 
-        <nav>
-            <ul className='flex space-x-8'>
-        {/* Right header (Hyperlenker til andre pages) => HJEM, BESTILL TIME, KONTAKT, PRISER, OM MEG*/}
-                <li><a href="/">Hjem</a></li>
-                <li><a href="/bestillTime">Bestill Time</a></li>
-                <li><a href="/kontakt">Kontakt</a></li>
-                <li><a href="/priser">Priser</a></li>
-                <li><a href="/om">Om Meg</a></li>
-            </ul>
-        </nav>
-    
+      <nav>
+        <ul className="flex space-x-8">
+          {/* Right header (Hyperlenker til andre pages) => HJEM, BESTILL TIME, KONTAKT, PRISER, OM MEG*/}
+          <li>
+            <a href="/">Hjem</a>
+          </li>
+          <li>
+            <a href="/bestillTime">Bestill Time</a>
+          </li>
+          <li>
+            <a href="/kontakt">Kontakt</a>
+          </li>
+          <li>
+            <a href="/priser">Priser</a>
+          </li>
+          <li>
+            <a href="/om">Om Meg</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default header
+export default header;
