2582885dd54bb556cda186a455cf2e56594394a8
Elias Opstad <eops@hotmail.no>
2025-07-18 13:47:53 +0200

    Added the Prices

--- src/app/priser/page.tsx
+++ src/app/priser/page.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React from "react";
 
 const Priser = () => {
   return (
-    <div>Priser</div>
-  )
-}
+    // Gestalt-coaching, 50 minutter, kr. 1250 kr (1000 + mva)
+    // Gestalt-coaching, 75 minutter, 1875 kr (1500 + mva)
+    // Avbestilling må skje minst 24 timer på forhånd for å unngå fakturering.
+    <>
+      <div>Priser</div>
+      <p>50 min: 1250 kr (1000 + mva)</p>
+      <p>75 min: 1875 kr (1500 + mva)</p>
+      <p>
+        Avbestilling må skje minst 24 timer på forhånd for å unngå fakturering.
+      </p>
+    </>
+  );
+};
 
-export default Priser
+export default Priser;
