7a4594938a68179072b5f507c13ba1ade0f4cb1b
Elias Opstad <eops@hotmail.no>
2025-07-18 15:49:03 +0200

    Fixed import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 import React from "react";
 import "@/styles/globals.css";
-import Header from "@/components/Header";
+import Header from "../components/Header";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
