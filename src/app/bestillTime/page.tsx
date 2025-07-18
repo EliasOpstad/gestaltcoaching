eb1f842a7092939daf0c95ee1b893d11097d06f1
Elias Opstad <eops@hotmail.no>
2025-07-18 13:48:19 +0200

    Formating of BestillTime

--- src/app/bestillTime/page.tsx
+++ src/app/bestillTime/page.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import '../../styles/globals.css'
+import React from "react";
+import "../../styles/globals.css";
 const BestillTime = () => {
   return (
-    <div className='w-[80%] mx-auto mt-30'>
-        <h1 className='text-3xl font-bold mb-4 text-center'>Bestill time</h1>
-        <div className='text-center'>Legg til booking system her</div>
+    <div className="w-[80%] mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-center">Bestill time</h1>
+      <div className="text-center">Legg til booking system her</div>
     </div>
+  );
+};
 
-  )
-}
-
-export default BestillTime
+export default BestillTime;
