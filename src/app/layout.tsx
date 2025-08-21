210b321109baf30d62b261cd811abcedfc7d2d9f
Elias Opstad <eops@hotmail.no>
2025-08-21 14:36:57 +0200

    Changed the Header import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 import React from "react";
-import Header from "../components/Header";
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
