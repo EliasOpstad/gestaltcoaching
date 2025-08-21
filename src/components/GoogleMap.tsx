d475f47fb0f423ea967aad6d1d1f123541094e06
Elias Opstad <eops@hotmail.no>
2025-08-21 23:23:14 +0200

    Changed the height from 600px to 400

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -5,7 +5,7 @@ const GoogleMap = () => {
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2231.7224143386566!2d10.710119177181676!3d59.91626746390157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416dd33f7f5f93%3A0xf1e92962d7ccb80d!2zQnlnZMO4eSBhbGzDqSAyMSwgMDI2MiBPc2xv!5e1!3m2!1sno!2sno!4v1752832172111!5m2!1sno!2sno"
       width="100%"
-      height="600"
+      height="400"
       className="rounded-lg mx-auto"
       loading="lazy"
     ></iframe>
