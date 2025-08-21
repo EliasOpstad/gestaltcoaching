98c7bc3eb792ff0a5d0218e336f882901c59ae09
Elias Opstad <eops@hotmail.no>
2025-08-21 23:23:41 +0200

    Replaced the text with lorem ipsum

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,21 +3,22 @@ import Link from "next/link";
 
 export default function AboutSection() {
   return (
-    <section id="about" className="w-full text-black py-20">
+    <section id="about" className="w-full text-black py-10">
       <div className="mx-auto max-w-6xl px-6 lg:grid lg:grid-cols-12 lg:gap-12 items-center">
         {/* Venstre: tekst */}
         <div className="lg:col-span-6 space-y-6">
           <h2 className="text-5xl font-serif leading-none">
-            Vår metode – din styrke
+            Gestaltcoach
+            <p>Cathrine Gåre Opstad</p>
           </h2>
 
           <p className="text-lg leading-relaxed text-black/90 max-w-xl">
-            <span className="block font-serif text-2xl mb-2"></span>
-            Gestaltorientert coaching er relasjonell, opplevelsesbasert og
-            prosessorientert. Vi utforsker sammen det som skjer her og nå.
-            Metoden er kreativ og eksperimenterende, med røtter i konfluent
-            pedagogikk. Målet er at du ser deg selv tydeligere og finner dine
-            egne valg – fordi <em>du sitter med svaret</em>.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
           </p>
 
           <div className="text-black/80 text-base">
