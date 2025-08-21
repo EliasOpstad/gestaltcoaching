7c8525a7475c0b22448a3a3dac314fc9e2a44c64
Elias Opstad <eops@hotmail.no>
2025-08-21 23:22:40 +0200

    Created a textbanner component to display the motto.

---
+++ src/components/Textbanner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Textbanner = () => {
+  return (
+    <div className="text-[#4d724d] text-3xl flex justify-center space-x-4 gap-8">
+      <p>«Du sitter med svaret»</p>
+      <p>·</p>
+      <p>«Vi gjør det sammen»</p>
+      <p>·</p>
+      <p>«Å se seg selv»</p>
+    </div>
+  );
+};
+
+export default Textbanner;
